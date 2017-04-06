bc49834a80b17b8ee8071c603c3cbf72cbbc7601
Jake T <jaketepper@gmail.com>
2017-04-06 16:25:10 -0400

    First Commit

---
+++ src/com/theironyard/charlotte/Game.java
@@ -0,0 +1,19 @@
+package com.theironyard.charlotte;
+
+/**
+ * Created by Jake on 4/6/17.
+ */
+public class Game {
+    Game(String name, String genre, String console, int year, int rating){
+        this.name = name;
+        this.genre = genre;
+        this.console = console;
+        this.year = year;
+        this.rating = rating;
+    }
+    public static String name;
+    String genre;
+    String console;
+    int year;
+    int rating;
+}
--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -1,8 +1,50 @@
 package com.theironyard.charlotte;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
+    public static HashMap<String, Game> Games = new HashMap<>();
+    public static Scanner inputScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Welcome to the Game Database. \n Here you can store information about video games.");
+        while (true) {
+            System.out.println("Main Menu: \n 1. Add a game \n 2. Update a game \n 3. List all items in database. \n 4. Info on a game.");
+            String rootEntry = inputScanner.nextLine();
+
+            if (rootEntry.equals("1")) {
+                System.out.println("Please enter the name of the game you would like to add to the database:");
+                String newName = Main.inputScanner.nextLine();
+                if (Main.Games.containsKey(newName)) {
+                    System.out.println("I'm sorry, but that game is already in the database. \n Returning to main menu... \n");
+                } else {
+                    System.out.println("Please enter the genre:");
+                    String enteredGenre = inputScanner.nextLine();
+                    System.out.println("Please enter the console the game is on:");
+                    String enteredConsole = inputScanner.nextLine();
+                    System.out.println("Please enter the year the game was released:");
+                    String enteredYear = inputScanner.nextLine();
+                    System.out.println("Please enter the rating of the game [1-5] star:");
+                    String enteredRating = inputScanner.nextLine();
+                    Game newGame = new Game(newName, enteredGenre, enteredConsole, Integer.valueOf(enteredYear), Integer.valueOf(enteredRating));
+                    newGame.name = newName;
+                    Main.Games.put(newGame.name, newGame);
+                    System.out.println("A new game has been added to the database.");
+                }
+            } else if (rootEntry.equals("2")) {
+                System.out.println("What is the name of the game you wish to update?");
+            } else if (rootEntry.equals("3")) {
+                System.out.println("Here are the games in the database:");
+                System.out.println(Games.keySet());
+            } else if (rootEntry.equals("4")) {
+                System.out.println("Please enter the name of the game.");
+                String infoInquiry = inputScanner.nextLine();
+                System.out.printf("Here is the info from the server: \n Name = %s \n Genre = %s \n Console = %s \n Release Date = %d \n Rating = %d \n ",Games.get(infoInquiry).name, Games.get(infoInquiry).genre, Games.get(infoInquiry).console, Games.get(infoInquiry).year, Games.get(infoInquiry).rating);
+            } else {
+            System.err.println("Invalid entry, human.");
+        }
+
+        }
     }
 }
